2c2cf357c65ed6b8cb2b87b27cd914480e2aba0f
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2025-11-12 21:50:57 -0800

    chore(formatting): forgot to format

--- modules/r/rofi/check.nix
+++ modules/r/rofi/check.nix
@@ -13,25 +13,26 @@ let
 
 in
 if builtins.elem pkgs.system self.wrapperModules.rofi.meta.platforms then
-pkgs.runCommand "rofi-test" { } ''
-  # Rofi attempts to create some directories when first ran which doesn't work in a nix build
-  export XDG_CACHE_HOME=/tmp
-  export XDG_RUNTIME_DIR=/tmp
+  pkgs.runCommand "rofi-test" { } ''
+    # Rofi attempts to create some directories when first ran which doesn't work in a nix build
+    export XDG_CACHE_HOME=/tmp
+    export XDG_RUNTIME_DIR=/tmp
 
-  res=$("${rofiWrapped}/bin/rofi" --help)
+    res=$("${rofiWrapped}/bin/rofi" --help)
 
-  if ! echo "$res" | grep -q '/nix/store/.*-config.rasi'; then
-    echo "Rofi doesn't see config"
+    if ! echo "$res" | grep -q '/nix/store/.*-config.rasi'; then
+      echo "Rofi doesn't see config"
+      touch $out
+      exit 1
+    fi
+
+    if ! echo "$res" | grep -q '/nix/store/.*-rofi-theme'; then
+      echo "Rofi doesn't see theme"
+      touch $out
+      exit 1
+    fi
+
     touch $out
-    exit 1
-  fi
-
-  if ! echo "$res" | grep -q '/nix/store/.*-rofi-theme'; then
-    echo "Rofi doesn't see theme"
-    touch $out
-    exit 1
-  fi
-
-  touch $out
-''
-else pkgs.runCommand "rofi-test-disabled" { } "touch $out"
+  ''
+else
+  pkgs.runCommand "rofi-test-disabled" { } "touch $out"
